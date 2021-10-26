e3e1e5c67b33e9b13d9a9dadcc5b2129082c1a67
ShankS3 <shashank.surwase@gmail.com>
2021-10-27 01:55:25 +0530

    Added FormBar component for satisfying FormBar test

--- src/components/FormBar.tsx
+++ src/components/FormBar.tsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useRef } from "react";
 import 'styles/formBar.css';
 
 const FormBar: React.FC<any> = () => {
-    return (<></>);
+    const inputRef = useRef<HTMLInputElement>(null);
+    const selectRef = useRef<HTMLSelectElement>(null);
+
+    return (
+        <form className="formbar">
+            <div className="input-container">
+                <input type="text" id="search-text" data-testid="search-text" placeholder='Search...' ref={inputRef} />
+                <select id="sort-select" data-testid="sort-select" ref={selectRef}>
+                    <option value="">-- Select --</option>
+                </select>
+            </div>
+        </form>
+    );
 }
 
 export default FormBar;
