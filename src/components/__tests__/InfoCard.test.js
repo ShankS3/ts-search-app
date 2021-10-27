3d720ab3a2f4e053c46f563ad275e2b8f821ceb4
ShankS3 <shashank.surwase@gmail.com>
2021-10-28 00:30:32 +0530

    Added test cases for card data display in info card

--- src/components/__tests__/InfoCard.test.js
+++ src/components/__tests__/InfoCard.test.js
@@ -3,17 +3,30 @@ import { render } from '@testing-library/react';
 import InfoCard from 'components/InfoCard';
 
 describe('Info Card', () => {
-    let getByTestId;
+    let getByTestId, rerender, container;
+    const cardProps = {
+        image: 'http://www.imgurl.com',
+        name: 'card name',
+        description: 'some description'
+    };
+
     beforeEach(() => {
-        ({ getByTestId } = render(<InfoCard />));
+        ({ getByTestId, rerender, container } = render(<InfoCard data={{}} />));
+    });
+
+    test('card should not render on empty prop', () => {
+        expect(container.childElementCount).toEqual(0);
     });
 
     test('card should render', () => {
+        rerender(<InfoCard data={cardProps} />);
         const card = getByTestId('card');
         expect(card).toBeInTheDocument();
+        expect(container.childElementCount).toEqual(1);
     })
 
     test('card should contain elements', () => {
+        rerender(<InfoCard data={cardProps} />);
         const card = getByTestId('card');
         const cardImg = getByTestId('card-img');
         const cardTitle = getByTestId('card-title');
@@ -21,5 +34,9 @@ describe('Info Card', () => {
         expect(card).toContainElement(cardImg);
         expect(card).toContainElement(cardTitle);
         expect(card).toContainElement(cardBody);
+        expect(cardImg).toHaveProperty('src', 'http://www.imgurl.com/');
+        expect(cardImg).toHaveProperty('alt', 'card name');
+        expect(cardTitle).toHaveTextContent('card name');
+        expect(cardBody).toHaveTextContent('some description');
     });
 });
