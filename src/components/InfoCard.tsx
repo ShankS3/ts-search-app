76b22e54e79bb861a8737000cefb82886e535556
ShankS3 <shashank.surwase@gmail.com>
2021-10-28 00:31:21 +0530

    Added conditional rendering in info card

--- src/components/InfoCard.tsx
+++ src/components/InfoCard.tsx
@@ -1,8 +1,26 @@
 import React from "react";
 
-const InfoCard: React.FC<any> = () => {
+interface Props {
+    data: {
+        image?: string;
+        name?: string;
+        description?: string;
+    }
+}
 
-    return (<></>);
+const InfoCard: React.FC<Props> = ({data}) => {
+
+    return Object.keys(data).length !== 0 ? (
+        <div className="card" data-testid="card">
+            <img src={data.image} alt={data.name} className="card-img" data-testid="card-img" />
+            <h4 className="card-title" data-testid="card-title">
+                {data.name}
+            </h4>
+            <div className="card-body" data-testid="card-body">
+                {data.description}
+            </div>
+        </div>
+    ) : null;
 }
 
 export default InfoCard;
