d153d8ee2e1fc51c026b836b663af5312df4bcfe
ShankS3 <shashank.surwase@gmail.com>
2021-10-28 01:03:13 +0530

    Added tests in app to render child components

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -2,8 +2,28 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from 'components/App';
 
-test('renders Feed', () => {
-  render(<App />);
-  expect(document.querySelector('h3')).toBeInTheDocument();
-  expect(document.querySelector('h3')).toHaveTextContent(/^Feed$/);
+describe('App', () => {
+  let getByTestId, container;
+  beforeEach(() => {
+    ({ getByTestId, container } = render(<App />));
+  });
+
+  test('renders App', () => {
+    expect(container.childElementCount).toEqual(1);
+    expect(getByTestId('App')).toBeInTheDocument();
+  });
+
+  test('renders Feed', () => {
+    const heading = getByTestId('page-title');
+    expect(heading).toBeInTheDocument();
+    expect(heading).toHaveTextContent(/^Feed$/);
+  });
+
+  test('renders Form bar', () => {
+    expect(getByTestId('search-text')).toBeInTheDocument();
+  });
+
+  test('renders card container', () => {
+    expect(getByTestId('card-container')).toBeInTheDocument();
+  });
 });
