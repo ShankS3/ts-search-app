e720854ee0c4e1d9c50e177ada715c276a9b69cf
ShankS3 <shashank.surwase@gmail.com>
2021-10-27 21:09:05 +0530

    Added tessts to update values in formbar

--- src/components/__tests__/FormBar.test.js
+++ src/components/__tests__/FormBar.test.js
@@ -1,20 +1,44 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import FormBar from 'components/FormBar';
 
+describe('Form Bar', () => {
+    let getByTestId, getByRole;
+    beforeEach(async () => {
+        ({ getByTestId, getByRole } = render(<FormBar />));
+    });
+
 test('renders input field', () => {
-    render(<FormBar />);
-    const searchInput = screen.getByTestId('search-text');
+    const searchInput = getByTestId('search-text');
+    const searchButton = getByTestId('search-button');
 
     expect(searchInput).toBeInTheDocument();
     expect(searchInput).toHaveAttribute('type', 'text');
     expect(searchInput).toHaveValue('');
+    expect(searchButton).toBeInTheDocument();
+    expect(searchButton).toHaveTextContent('search');
+});
+
+test('clicking the search button', () => {
+    const searchInput = getByTestId('search-text');
+    const searchButton = getByTestId('search-button');
+    userEvent.type(searchInput, 'abc');
+    userEvent.click(searchButton);
+    expect(searchInput.value).toEqual('abc');
 });
 
 test('renders select field', () => {
-    render(<FormBar />);
-    const sortSelect = screen.getByTestId('sort-select');
+    const sortSelect = getByTestId('sort-select');
 
     expect(sortSelect).toBeInTheDocument();
-    expect(sortSelect).toHaveValue('');
+    expect(sortSelect).toHaveValue('sort1');
+    console.log("consoling sort select", sortSelect.value);
+    userEvent.selectOptions(sortSelect, ['sort2']);
+    console.log("consoling sort select", sortSelect.value);
+    expect(sortSelect.value).toEqual('sort2');
+    expect(getByRole('option', {name: 'sort1'}).selected).toBe(false)
+    expect(getByRole('option', {name: 'sort2'}).selected).toBe(true)
+    expect(getByRole('option', {name: 'sort3'}).selected).toBe(false)
+});
 });
