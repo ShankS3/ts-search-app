147b3b9846271b6f9b1d23cf9aa1f0d1aded219a
ShankS3 <shashank.surwase@gmail.com>
2021-10-28 02:51:22 +0530

    Updated DataTable as per test cases

--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
 
-const DataTable: React.FC<any> = () => {
-    return (<></>);
+interface column {
+    header: string;
+    name: string;
+}
+interface Props {
+    columns: column[]
+}
+
+const DataTable: React.FC<Props> = ({columns}) => {
+    return (
+        <div className="dataTable-container" data-testid="dataTable-container">
+            <table className="dataTable" data-testid="dataTable">
+                <thead className="dataTable-head" data-testid="dataTable-head">
+                    <tr>
+                        {columns.map(col => <th key={col.name}>{col.header}</th>)}
+                    </tr>
+                </thead>
+            </table>
+        </div>
+    );
 }
 
 export default DataTable;
