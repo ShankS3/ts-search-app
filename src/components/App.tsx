e64ee3a9b026cb4c9c94e550e6f79a82bd2d2cc6
ShankS3 <shashank.surwase@gmail.com>
2021-10-28 01:03:55 +0530

    Added child components in app

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+
+import FormBar from 'components/FormBar';
 import 'styles/App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <h3>Feed</h3>
+    <div className="App" data-testid="App">
+      <h3 className="page-title" data-testid="page-title">Feed</h3>
+      <FormBar />
+      <div className="card-container" data-testid="card-container">
+      </div>
     </div>
   );
 }
