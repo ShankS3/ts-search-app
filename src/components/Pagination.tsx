5c5256ca052bb15af34df033ccd95ccb58379f4b
ShankS3 <shashank.surwase@gmail.com>
2021-10-28 14:52:32 +0530

    Added basic components in Pagination component

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -5,7 +5,13 @@ interface PaginationProps {
 }
  
 const Pagination: React.FC<PaginationProps> = () => {
-    return (<></>);
+    return (
+        <div className="pagination-wrapper" data-testid="pagination-wrapper">
+            <button className="previous-button" data-testid="previous-button">&lt;</button>
+            <button className="page-button" data-testid="page-button">1</button>
+            <button className="next-button" data-testid="next-button">&gt;</button>
+        </div>
+    );
 }
  
 export default Pagination;
