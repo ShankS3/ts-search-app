c4a912bc3167953fbf290c1a2cbdb782feb0bf26
ShankS3 <shashank.surwase@gmail.com>
2021-10-29 15:04:25 +0530

    Added types for feed reducer

--- src/constants/types.ts
+++ src/constants/types.ts
@@ -8,12 +8,34 @@ export interface data {
 }
 
 export interface PaginationProps {
-    page: number,
-    pageSize: number,
+    page: number;
+    pageSize: number;
     handlePagination: (page: number) => void 
 }
-
 
 export type columnArray = column[];
 
 export type dataArray = data[];
+
+export interface FeedData {
+    page: number;
+    pageSize: number;
+    searchText: string;
+    sortBy: string[];
+    data: dataArray;
+    totalCount: number;
+};
+
+export interface FeedErrorData {
+    page: number;
+    pageSize: number;
+    searchText: string;
+    sortBy: string[];
+    errorMessage: string
+};
+
+export interface FeedState extends FeedData {
+    loader: boolean;
+    error: boolean;
+    errorMessage: string;
+};
