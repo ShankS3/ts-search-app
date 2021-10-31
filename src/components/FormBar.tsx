44574b706a060a35f5fa2975370cb86e2834bd19
ShankS3 <shashank.surwase@gmail.com>
2021-10-31 19:03:26 +0530

    Updated formbar

--- src/components/FormBar.tsx
+++ src/components/FormBar.tsx
@@ -1,16 +1,27 @@
+import { FeedFormData } from "constants/types";
 import React, { FormEvent, useRef } from "react";
 import Icons from 'react-fa';
 
 import 'styles/FormBar.css';
 
-const FormBar: React.FC<any> = () => {
-    const inputRef = useRef<HTMLInputElement>(null);
-    const selectRef = useRef<HTMLSelectElement>(null);
+interface FormBarProps {
+    page: number,
+    submitAction: (formData: FeedFormData) => {}
+}
+
+const FormBar: React.FC<FormBarProps> = ({page, submitAction}) => {
+    const searchInputRef = useRef<HTMLInputElement>(null);
+    const sortSelectRef = useRef<HTMLSelectElement>(null);
 
     const submitFilters = (event: FormEvent): void => {
         event.preventDefault();
-        console.log(inputRef.current?.value);
-        console.log(selectRef.current?.value);
+
+        let formData: FeedFormData = {
+            page,
+            searchText: searchInputRef.current?.value || '',
+            sortBy: sortSelectRef.current?.value || ''
+        };
+        submitAction(formData);
     }
 
     return (
@@ -18,13 +29,24 @@ const FormBar: React.FC<any> = () => {
             <div className="input-container">
                 <div>
                     <form onSubmit={submitFilters}>
-                        <input type="text" id="search-text" data-testid="search-text" placeholder='Search...' ref={inputRef} />
-                        <button id="search-button" data-testid="search-button"><Icons name="search" size="lg" /></button>
+                        <input 
+                            type="text" 
+                            id="search-text" 
+                            data-testid="search-text" 
+                            placeholder='Search...' 
+                            ref={searchInputRef} 
+                        />
+                        <button 
+                            id="search-button" 
+                            data-testid="search-button"
+                        >
+                            <Icons name="search" size="lg" />
+                        </button>
                     </form>
                 </div>
                 <div>
                     <label htmlFor="sort-select">Sort by </label>
-                    <select id="sort-select" data-testid="sort-select" onChange={submitFilters} ref={selectRef}>
+                    <select id="sort-select" data-testid="sort-select" onChange={submitFilters} ref={sortSelectRef}>
                         <option value="">select option</option>
                         <option value="name,asc">Name - Ascending</option>
                         <option value="name,desc">Name - Descending</option>
