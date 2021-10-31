4b1a691f1b0a1f42863d4c56ad8a9e4699d5ceb7
ShankS3 <shashank.surwase@gmail.com>
2021-10-31 23:58:07 +0530

    Added redux-persist

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,37 @@
-
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER 
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import rootReducer from 'reducers';
 
+const persistConfig = {
+  key: "ts-search",
+  version: 1,
+  storage
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+      }
+    })
 });
+
+export let persistor = persistStore(store);
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof rootReducer>;
