015aaf35d28fa0cb3b845f7d7dd505b5d728e54c
ShankS3 <shashank.surwase@gmail.com>
2021-10-31 20:21:47 +0530

    Added test to render form components with props

--- src/components/__tests__/FormBar.test.js
+++ src/components/__tests__/FormBar.test.js
@@ -4,52 +4,64 @@ import userEvent from '@testing-library/user-event';
 import FormBar from 'components/FormBar';
 
 describe('Form Bar', () => {
-    let getByTestId, getByRole;
+    let getByTestId, getByRole, rerender;
     let page = 1;
     let submitAction = jest.fn();
 
     beforeEach(() => {
-        ({ getByTestId, getByRole } = render(<FormBar page={page} submitAction={submitAction} />));
+        ({ getByTestId, getByRole, rerender } = render(<FormBar page={page} submitAction={submitAction} searchText="" sortBy="" />));
     });
 
-test('renders input field', () => {
-    const searchInput = getByTestId('search-text');
-    const searchButton = getByTestId('search-button');
+    test('renders input field', () => {
+        const searchInput = getByTestId('search-text');
+        const searchButton = getByTestId('search-button');
 
-    expect(searchInput).toBeInTheDocument();
-    expect(searchInput).toHaveAttribute('type', 'text');
-    expect(searchInput).toHaveValue('');
-    expect(searchButton).toBeInTheDocument();
-});
+        expect(searchInput).toBeInTheDocument();
+        expect(searchInput).toHaveAttribute('type', 'text');
+        expect(searchInput).toHaveValue('');
+        expect(searchButton).toBeInTheDocument();
+    });
 
-test('clicking the search button', () => {
-    const searchInput = getByTestId('search-text');
-    const searchButton = getByTestId('search-button');
-    userEvent.type(searchInput, 'abc');
-    userEvent.click(searchButton);
-    expect(searchInput.value).toEqual('abc');
-    expect(submitAction).toBeCalledWith({
-        searchText: 'abc',
-        sortBy: '',
-        page
+    test('clicking the search button', () => {
+        const searchInput = getByTestId('search-text');
+        const searchButton = getByTestId('search-button');
+        userEvent.type(searchInput, 'abc');
+        userEvent.click(searchButton);
+        expect(searchInput.value).toEqual('abc');
+        expect(submitAction).toBeCalledWith({
+            searchText: 'abc',
+            sortBy: '',
+            page
+        })
+    });
+
+    test('renders select field', () => {
+        const sortSelect = getByTestId('sort-select');
+
+        expect(sortSelect).toBeInTheDocument();
+        expect(sortSelect).toHaveValue('');
+        userEvent.selectOptions(sortSelect, ['name,desc']);
+        expect(submitAction).toBeCalledWith({
+            searchText: '',
+            sortBy: 'name,desc',
+            page
+        });
+        expect(sortSelect.value).toEqual('name,desc');
+        expect(getByRole('option', {name: 'Name - Ascending'}).selected).toBe(false);
+        expect(getByRole('option', {name: 'Name - Descending'}).selected).toBe(true);
+        expect(getByRole('option', {name: 'Last updated - Ascending'}).selected).toBe(false);
+        expect(getByRole('option', {name: 'Last updated - Descending'}).selected).toBe(false);
+    });
+
+    test('should render with store props', () => {
+        const searchInput = getByTestId('search-text');
+        const sortSelect = getByTestId('sort-select');
+        let searchText = "lord of the rings";
+        let sortBy = "name,desc";
+
+        rerender(<FormBar page={page} searchText={searchText} sortBy={sortBy} submitAction={submitAction} />);
+        
+        expect(searchInput.value).toEqual(searchText);
+        expect(sortSelect).toHaveValue(sortBy);
     })
-});
-
-test('renders select field', () => {
-    const sortSelect = getByTestId('sort-select');
-
-    expect(sortSelect).toBeInTheDocument();
-    expect(sortSelect).toHaveValue('');
-    userEvent.selectOptions(sortSelect, ['name,desc']);
-    expect(sortSelect.value).toEqual('name,desc');
-    expect(getByRole('option', {name: 'Name - Ascending'}).selected).toBe(false)
-    expect(getByRole('option', {name: 'Name - Descending'}).selected).toBe(true)
-    expect(getByRole('option', {name: 'Last updated - Ascending'}).selected).toBe(false)
-    expect(getByRole('option', {name: 'Last updated - Descending'}).selected).toBe(false)
-    expect(submitAction).toBeCalledWith({
-        searchText: '',
-        sortBy: 'name,desc',
-        page
-    })
-});
 });
